2b17a211db5318e14576f8ede6ba36198de6be56
Jeff Clarenz <jeff.clarenz@gmail.com>
2022-05-01 03:36:12 +0800

    ties

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -61,9 +61,9 @@ const Result: FC<ResultProps> = (props) => {
     };
 
     if (resultsAvailable && finalResult.length > 0) {
-      if (i.onResult) {
-        if (typeof i.onResult === "function") {
-          i.onResult();
+      if (finalResult[0].onResult) {
+        if (typeof finalResult[0].onResult === "function") {
+          finalResult[0].onResult();
         } else {
           generalOnResultFunction();
         }
@@ -75,64 +75,67 @@ const Result: FC<ResultProps> = (props) => {
 
   if (resultsAvailable && finalResult.length > 0) {
     return (
-      finalResult.map(function(i){
-        return (
-          <Element className="rbq_result_outer_container" name="Result">
-          <div className="rbq_result_header">
-            <h2>{title}</h2>
-          </div>
-          <div className="rbq_result_inner_container">
-            <div className="rbq_result_inner_description_container">
-              <h3 className="rbq_result_inner_description_header">
-                {i.title}
-              </h3>
-              <p className="rbq_result_inner_description">
-                {i.description}
-              </p>
-            </div>
-            {i.resultImageSrc && (
-              <div className="rbq_result_inner_image_container">
-                <img
-                  className="rbq_result_inner_image"
-                  alt="Buzzfeed Quiz Result Image"
-                  src={i.resultImageSrc}
-                />
-                {i.imageAttribution ? (
-                  <span className="rbq_result_attribution_text">
-                    {i.imageAttribution}
-                  </span>
-                ) : null}
+      <Element className="rbq_result_outer_container" name="Result">
+        <div className="rbq_result_header">
+          <h2>{title}</h2>
+        </div>
+        
+        {finalResult.map((i) => {
+            return (
+              <div className="rbq_result_inner_container">
+                <div className="rbq_result_inner_description_container">
+                  <h3 className="rbq_result_inner_description_header">
+                    {i.title}
+                  </h3>
+                  <p className="rbq_result_inner_description">
+                    {i.description}
+                  </p>
+                </div>
+                {i.resultImageSrc && (
+                  <div className="rbq_result_inner_image_container">
+                    <img
+                      className="rbq_result_inner_image"
+                      alt="Buzzfeed Quiz Result Image"
+                      src={i.resultImageSrc}
+                    />
+                    {i.imageAttribution ? (
+                      <span className="rbq_result_attribution_text">
+                        {i.imageAttribution}
+                      </span>
+                    ) : null}
+                  </div>
+                )}
               </div>
+            )
+          })
+        }
+
+        <div className="rbq_result_footer">
+          <button className="rbq_retake_quiz_button" onClick={handleRetakeQuiz}>
+            Retake
+          </button>
+          <ul className="rbq_share_links_list" aria-label="share">
+            {copyShareButton && (
+              <CopyLinkButton
+                shareLinkClicked={shareLinkClicked}
+                changeShareLinkClicked={changeShareLinkClicked}
+                shareLinkAnimatingOut={shareLinkAnimatingOut}
+                copyShareLink={copyShareLink}
+              />
             )}
-          </div>
-          <div className="rbq_result_footer">
-            <button className="rbq_retake_quiz_button" onClick={handleRetakeQuiz}>
-              Retake
-            </button>
-            <ul className="rbq_share_links_list" aria-label="share">
-              {copyShareButton && (
-                <CopyLinkButton
-                  shareLinkClicked={shareLinkClicked}
-                  changeShareLinkClicked={changeShareLinkClicked}
-                  shareLinkAnimatingOut={shareLinkAnimatingOut}
-                  copyShareLink={copyShareLink}
-                />
-              )}
-              {twitterShareButton && (
-                <TwitterButton
-                  twitterShareLink={twitterShareLink}
-                  twitterShareText={twitterShareText}
-                  twitterShareHashtags={twitterShareHashtags}
-                />
-              )}
-              {facebookShareButton && (
-                <FacebookButton facebookShareLink={facebookShareLink} />
-              )}
-            </ul>
-          </div>
-        </Element>
-        )
-      })
+            {twitterShareButton && (
+              <TwitterButton
+                twitterShareLink={twitterShareLink}
+                twitterShareText={twitterShareText}
+                twitterShareHashtags={twitterShareHashtags}
+              />
+            )}
+            {facebookShareButton && (
+              <FacebookButton facebookShareLink={facebookShareLink} />
+            )}
+          </ul>
+        </div>
+      </Element>
     );
   } else {
     return null;
